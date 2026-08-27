4e2deec2380808c74f3cd58cad64bb624eabd6ec
Chris Birster <christopher.birster@gmail.com>
2026-08-27 00:51:26 -0400

    Demonstrate Clipboard module

--- examples/hello-world/src/App.tsx
+++ examples/hello-world/src/App.tsx
@@ -1,9 +1,11 @@
 import { createSignal } from 'solid-js';
+import { Clipboard } from '@stingjs/clipboard';
 import { Haptics } from '@stingjs/haptics';
 import { Button, Text, View } from '@stingjs/native';
 
 export default function App() {
   const [count, setCount] = createSignal(0);
+  const [clipboardStatus, setClipboardStatus] = createSignal('Clipboard ready');
 
   return (
     <View style={{ flexDirection: 'column', gap: 12, padding: 24 }}>
@@ -16,6 +18,15 @@ export default function App() {
       >
         Add
       </Button>
+      <Button
+        onPress={() => {
+          Clipboard.setString(`Count: ${count()}`);
+          setClipboardStatus(`Copied Count: ${count()}`);
+        }}
+      >
+        Copy count
+      </Button>
+      <Text>{clipboardStatus()}</Text>
     </View>
   );
 }
