e5e92b3434e78e0353fcd852b5f1a6e5da5f79a1
Chris Birster <christopher.birster@gmail.com>
2026-08-27 00:48:30 -0400

    Include Clipboard Android module

--- examples/hello-world/android/settings.gradle.kts
+++ examples/hello-world/android/settings.gradle.kts
@@ -19,11 +19,13 @@ include(
     ":app",
     ":sting-runtime",
     ":sting-haptics",
+    ":sting-clipboard",
     ":sting-runtime-quickjs-candidate",
     ":sting-runtime-quickjs-ng-candidate",
 )
 
 project(":sting-runtime").projectDir = file("../../../native/android")
 project(":sting-haptics").projectDir = file("../../../packages/modules/haptics/android")
+project(":sting-clipboard").projectDir = file("../../../packages/modules/clipboard/android")
 project(":sting-runtime-quickjs-candidate").projectDir = file("../../../runtime/prototypes/quickjs/android")
 project(":sting-runtime-quickjs-ng-candidate").projectDir = file("../../../runtime/prototypes/quickjs-ng/android")
