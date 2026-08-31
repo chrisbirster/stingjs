92ecf53a6a18e55813164f350e30fb58307ceded
Chris Birster <christopher.birster@gmail.com>
2026-08-30 21:23:26 -0400

    fix(modules): align Audio with Swift 5 package mode

--- packages/modules/audio/Package.swift
+++ packages/modules/audio/Package.swift
@@ -1,9 +1,26 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.10
+
 import PackageDescription
+
 let package = Package(
     name: "StingAudio",
-    platforms: [.macOS(.v13), .iOS(.v16)],
-    products: [.library(name: "StingAudio", targets: ["StingAudio"])],
-    dependencies: [.package(path: "../../../native/ios")],
-    targets: [.target(name: "StingAudio", dependencies: [.product(name: "StingRuntime", package: "ios")], path: "ios")]
+    platforms: [
+        .iOS(.v16)
+    ],
+    products: [
+        .library(name: "StingAudio", targets: ["StingAudio"])
+    ],
+    dependencies: [
+        .package(path: "../../../native/ios")
+    ],
+    targets: [
+        .target(
+            name: "StingAudio",
+            dependencies: [
+                .product(name: "StingRuntime", package: "ios")
+            ],
+            path: "ios"
+        )
+    ],
+    swiftLanguageVersions: [.v5]
 )
