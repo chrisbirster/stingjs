f08974ce58f37d903405270804d3d9396534b2bb
Chris Birster <christopher.birster@gmail.com>
2026-08-30 21:22:56 -0400

    fix(modules): align Location with Swift 5 package mode

--- packages/modules/location/Package.swift
+++ packages/modules/location/Package.swift
@@ -1,16 +1,26 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.10
+
 import PackageDescription
 
 let package = Package(
     name: "StingLocation",
-    platforms: [.macOS(.v13), .iOS(.v16)],
-    products: [.library(name: "StingLocation", targets: ["StingLocation"])],
-    dependencies: [.package(path: "../../../native/ios")],
+    platforms: [
+        .iOS(.v16)
+    ],
+    products: [
+        .library(name: "StingLocation", targets: ["StingLocation"])
+    ],
+    dependencies: [
+        .package(path: "../../../native/ios")
+    ],
     targets: [
         .target(
             name: "StingLocation",
-            dependencies: [.product(name: "StingRuntime", package: "ios")],
+            dependencies: [
+                .product(name: "StingRuntime", package: "ios")
+            ],
             path: "ios"
         )
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
