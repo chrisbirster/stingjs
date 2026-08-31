b924b3406022cca27540984068dff8fccac0deee
Chris Birster <christopher.birster@gmail.com>
2026-08-30 21:23:18 -0400

    fix(modules): align Notifications with Swift 5 package mode

--- packages/modules/notifications/Package.swift
+++ packages/modules/notifications/Package.swift
@@ -1,9 +1,26 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.10
+
 import PackageDescription
+
 let package = Package(
     name: "StingNotifications",
-    platforms: [.macOS(.v13), .iOS(.v16)],
-    products: [.library(name: "StingNotifications", targets: ["StingNotifications"])],
-    dependencies: [.package(path: "../../../native/ios")],
-    targets: [.target(name: "StingNotifications", dependencies: [.product(name: "StingRuntime", package: "ios")], path: "ios")]
+    platforms: [
+        .iOS(.v16)
+    ],
+    products: [
+        .library(name: "StingNotifications", targets: ["StingNotifications"])
+    ],
+    dependencies: [
+        .package(path: "../../../native/ios")
+    ],
+    targets: [
+        .target(
+            name: "StingNotifications",
+            dependencies: [
+                .product(name: "StingRuntime", package: "ios")
+            ],
+            path: "ios"
+        )
+    ],
+    swiftLanguageVersions: [.v5]
 )
