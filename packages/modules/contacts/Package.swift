ef3f352fa3f67c218c1a4f0f4af422ad983f228d
Chris Birster <christopher.birster@gmail.com>
2026-08-30 21:23:03 -0400

    fix(modules): align Contacts with Swift 5 package mode

--- packages/modules/contacts/Package.swift
+++ packages/modules/contacts/Package.swift
@@ -1,10 +1,26 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.10
+
 import PackageDescription
 
 let package = Package(
     name: "StingContacts",
-    platforms: [.macOS(.v13), .iOS(.v16)],
-    products: [.library(name: "StingContacts", targets: ["StingContacts"])],
-    dependencies: [.package(path: "../../../native/ios")],
-    targets: [.target(name: "StingContacts", dependencies: [.product(name: "StingRuntime", package: "ios")], path: "ios")]
+    platforms: [
+        .iOS(.v16)
+    ],
+    products: [
+        .library(name: "StingContacts", targets: ["StingContacts"])
+    ],
+    dependencies: [
+        .package(path: "../../../native/ios")
+    ],
+    targets: [
+        .target(
+            name: "StingContacts",
+            dependencies: [
+                .product(name: "StingRuntime", package: "ios")
+            ],
+            path: "ios"
+        )
+    ],
+    swiftLanguageVersions: [.v5]
 )
