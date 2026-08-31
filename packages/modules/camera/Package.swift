dcb74209df6fc4c0bd58a149664116442e6864cc
Chris Birster <christopher.birster@gmail.com>
2026-08-30 21:23:10 -0400

    fix(modules): align Camera with Swift 5 package mode

--- packages/modules/camera/Package.swift
+++ packages/modules/camera/Package.swift
@@ -1,9 +1,26 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.10
+
 import PackageDescription
+
 let package = Package(
     name: "StingCamera",
-    platforms: [.macOS(.v13), .iOS(.v16)],
-    products: [.library(name: "StingCamera", targets: ["StingCamera"])],
-    dependencies: [.package(path: "../../../native/ios")],
-    targets: [.target(name: "StingCamera", dependencies: [.product(name: "StingRuntime", package: "ios")], path: "ios")]
+    platforms: [
+        .iOS(.v16)
+    ],
+    products: [
+        .library(name: "StingCamera", targets: ["StingCamera"])
+    ],
+    dependencies: [
+        .package(path: "../../../native/ios")
+    ],
+    targets: [
+        .target(
+            name: "StingCamera",
+            dependencies: [
+                .product(name: "StingRuntime", package: "ios")
+            ],
+            path: "ios"
+        )
+    ],
+    swiftLanguageVersions: [.v5]
 )
